1e50cf1973cee1d23dadae86b2745e18820eae37
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-10-04 07:52:31 +0000

    not finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,13 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
   end
 
   private
