19793ed8d925e5fe71cad48dcb4737ad2f930c88
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-10-05 05:08:35 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,9 @@ class BooksController < ApplicationController
   def index
     to = Time.current.at_end_of_day
     from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-             sort_by{|x|
-                     x.favorited_users.includes(:favorites).size}.reverse
+    @books = Book.all.sort{|a,b|
+     b.favorites.where(created_at: from...to).size <=>
+     a.favorites.where(created_at: from...to).size}
     @book = Book.new
   end
 
